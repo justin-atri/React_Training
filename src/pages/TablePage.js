7bd11405c145311f9ae67bf1886acd3f89b0c038
Justin K. Jang <justinj@atrihub.io>
2022-09-29 11:15:26 -0700

    refactor(training): React Demo
    *Important*
    Jia-Shing showed me a proper way of code refactoring,
    in terms of creating a component in the same file, without deleting anything.
    This way I can test out safely and reduce code duplicates.
    
    This example shows how I can reduce useState&useEffect duplicates by creating a CategoryTable component,
    which also allows me to create many more categories by reusing this component.
    
    [ICTRAINING-1248]

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -4,51 +4,46 @@ import { Link } from "react-router-dom";
 import Table from "../components/Table";
 
 const TablePage = () => {
-  const [loadedMiscTable, setLoadedMiscTable] = useState();
-  const [loadedFoodTable, setLoadedFoodTable] = useState();
-
-  // Load Misc table on mount
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("/misc");
-
-        const miscTable = response.data;
-
-        setLoadedMiscTable(miscTable);
-      } catch (error) {}
-    };
-
-    fetchData();
-  }, []);
-
-  // Load Food table on mount
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("/food");
-
-        const foodTable = response.data;
-
-        setLoadedFoodTable(foodTable);
-      } catch (error) {}
-    };
-
-    fetchData();
-  }, []);
-
   return (
     <React.Fragment>
       <Link to="/create">
         <button>+</button>
       </Link>
 
-      {/* Only render Table when loadedTable exist */}
-      {loadedFoodTable && <Table tableData={loadedFoodTable} category="Food" />}
-      {loadedMiscTable && (
-        <Table tableData={loadedMiscTable} category="Miscellaneous" />
-      )}
+      {/* pass in the paramiter foward slash together in this case */}
+      <CategoryTable category="Food" apiEndpoint="/food" />
+      <CategoryTable category="Miscellaneous" apiEndpoint="/misc" />
     </React.Fragment>
+  );
+};
+
+const CategoryTable = ({ category, apiEndpoint }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // isLoading
+      try {
+        const response = await axios.get(`${apiEndpoint}`);
+
+        setTableData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, [apiEndpoint]);
+
+  if (isLoading) return <div>Loading Category: {category}</div>;
+
+  return (
+    <>
+      <Table tableData={tableData} category={category} />
+    </>
   );
 };
 
