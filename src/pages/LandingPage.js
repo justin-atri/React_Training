fba8f4151456cbf27865fde7d2d255c149bc62f2
Justin K. Jang <justinj@atrihub.io>
2022-09-30 04:23:51 -0700

    feat(training): React Demo
    Create styling for Landing Page.
    Code refactoring and clean up needed.
    
    [ICTRAINING-1252]

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
-import { Button } from "reactstrap";
 import landingImg from "../assets/undraw_join_re_w1lh.svg";
 import featherPen from "../assets/feather_pen.svg";
 
@@ -40,55 +39,69 @@ const LandingPage = () => {
       </section>
 
       <section>
-        <img src={featherPen} alt="feather pen" />
-        <h1>Trace</h1>
-        <form onSubmit={handleSubmit(beginTraceHandler)}>
-          <h5>Lets start by choosing you area of interests!</h5>
-          <label htmlFor="checkbox-1">
-            <input
-              type="checkbox"
-              name="checkbox-1"
-              value="Track record of my spendings"
-              id="checkbox-1"
-              {...register("checkbox-1")}
-            />
-            Track record of my spendings
-          </label>
-          <label htmlFor="checkbox-2">
-            <input
-              type="checkbox"
-              name="checkbox-2"
-              value="Build healthy spending habit"
-              id="checkbox-2"
-              {...register("checkbox-2")}
-            />
-            Build healthy spending habit
-          </label>
-          <label htmlFor="checkbox-3">
-            <input
-              type="checkbox"
-              name="checkbox-3"
-              value="Control over my finance"
-              id="checkbox-3"
-              {...register("checkbox-3")}
-            />
-            Control over my finance
-          </label>
-          <label htmlFor="checkbox-4">
-            <input
-              type="checkbox"
-              name="checkbox-4"
-              value="Investing & saving"
-              id="checkbox-4"
-              {...register("checkbox-4")}
-            />
-            Investing & saving
-          </label>
+        <div className="trace-background">
+          <div className="d-flex justify-content-center header_container">
+            <div className="trace-logo">
+              <img src={featherPen} alt="feather pen" />
+            </div>
 
-          <Button type="submit" color="primary" disabled={isSubmitting}>
-            Begin Trace!
-          </Button>
-        </form>
+            <h1>Trace</h1>
+          </div>
+
+          <form
+            onSubmit={handleSubmit(beginTraceHandler)}
+            className="form_container"
+          >
+            <div className="survey_container">
+              <h5>Lets start by choosing you area of interests!</h5>
+              <div className="checkbox_container">
+                <label htmlFor="checkbox-1">
+                  <input
+                    type="checkbox"
+                    name="checkbox-1"
+                    value="Track record of my spendings"
+                    id="checkbox-1"
+                    {...register("checkbox-1")}
+                  />
+                  Track record of my spendings
+                </label>
+                <label htmlFor="checkbox-2">
+                  <input
+                    type="checkbox"
+                    name="checkbox-2"
+                    value="Build healthy spending habit"
+                    id="checkbox-2"
+                    {...register("checkbox-2")}
+                  />
+                  Build healthy spending habit
+                </label>
+                <label htmlFor="checkbox-3">
+                  <input
+                    type="checkbox"
+                    name="checkbox-3"
+                    value="Control over my finance"
+                    id="checkbox-3"
+                    {...register("checkbox-3")}
+                  />
+                  Control over my finance
+                </label>
+                <label htmlFor="checkbox-4">
+                  <input
+                    type="checkbox"
+                    name="checkbox-4"
+                    value="Investing & saving"
+                    id="checkbox-4"
+                    {...register("checkbox-4")}
+                  />
+                  Investing & saving
+                </label>
+              </div>
+            </div>
+            <button type="submit" disabled={isSubmitting}>
+              Begin Trace!
+            </button>
+          </form>
+        </div>
       </section>
     </React.Fragment>
   );
